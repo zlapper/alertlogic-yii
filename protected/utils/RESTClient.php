b0676b866f6ff802fc44a55f179486f0262198d7
Daniel Garcia <zlapper@gmail.com>
2013-08-01 12:11:42 -0500

    Improved the separation layers between the REST and TMDB clients, and added documentation comments using phpdoc.

--- protected/utils/RESTClient.php
+++ protected/utils/RESTClient.php
@@ -1,75 +1,176 @@
 <?php
-	// Simple JSON REST Client (using curl)
-	// Author: Daniel Garcia
-	// Based on: http://snipplr.com/view/44760/
+/**
+* RESTClient
+*
+* RESTClient: Simple JSON REST Client, based on http://snipplr.com/view/44760/
+*
+* @uses      curl
+*
+* @author    Daniel Garcia <daniel@danielgarcia.co>
+*/
+class RESTClient {
+	/**
+	 * $handle
+	 *
+	 * The CURL handle.
+	 *
+	 * @var curl
+	 *
+	 * @access public
+	 */
+	public $handle;
 
-	class RESTClient {
-		public $handle;
+	/**
+	 * $response
+	 *
+	 * HTTP response from the current session.
+	 *
+	 * @var object(stdClass)
+	 *
+	 * @access public
+	 */
+	public $response;
 
-		public $response;
-		public $code;
-		public $error;
+	/**
+	 * $http_code
+	 *
+	 * HTTP response code from the current session.
+	 *
+	 * @var int
+	 *
+	 * @access public
+	 */
+	public $http_code;
 
-		public $api_root_url;
-		public $api_key;
+	/**
+	 * $error
+	 *
+	 * Last error message for the current session.
+	 *
+	 * @var string
+	 *
+	 * @access public
+	 */
+	public $error;
 
-		//Constructor of the class
-		public function __construct($api_root_url, $api_key) {
-			$this->api_root_url = $api_root_url;
-			$this->api_key = $api_key;
-			$this->init();
-		}
-
-		private function init() {
-			$headers = array(
-				'Accept: application/json',
-				'Content-Type: application/json',
-			);
-			$this->handle = curl_init();
-			curl_setopt($this->handle, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($this->handle, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($this->handle, CURLOPT_SSL_VERIFYHOST, false);
-			curl_setopt($this->handle, CURLOPT_SSL_VERIFYPEER, false);
-		}
-
-		private function close() {
-			curl_close($this->handle);
-		}
-
-		private function exec() {
-			$this->response = json_decode(curl_exec($this->handle));
-			$this->code = curl_getinfo($this->handle, CURLINFO_HTTP_CODE);
-			$this->error = curl_error($this->handle);
-		}
-
-		public function GET($service_name, $query_string='') {
-			// $url_params = urlencode($query_string).'&api_key='.$this->api_key;
-			$url_params = $query_string.'&api_key='.$this->api_key;
-			curl_setopt($this->handle, CURLOPT_URL, $this->api_root_url.$service_name.'?'.$url_params);
-			$this->exec();
-		}
-
-		public function POST($service_name, $data_array) {
-			$data_array['api_key']=$this->api_key;
-			$data = json_encode($data_array);
-			curl_setopt($this->handle, CURLOPT_URL, $this->api_root_url.$service_name);
-			curl_setopt($this->handle, CURLOPT_POST, true);
-			curl_setopt($this->handle, CURLOPT_POSTFIELDS, $data);
-			$this->exec();
-		}
-
-		public function PUT($service_name, $data_array) {
-			$data_array['api_key']=$this->api_key;
-			$data = json_encode($data_array);
-			curl_setopt($this->handle, CURLOPT_URL, $this->api_root_url.$service_name);
-			curl_setopt($this->handle, CURLOPT_CUSTOMREQUEST, 'PUT');
-			curl_setopt($this->handle, CURLOPT_POSTFIELDS, $data);
-			$this->exec();
-		}
-
-		public function DELETE($service_name) {
-			curl_setopt($this->handle, CURLOPT_CUSTOMREQUEST, 'DELETE');
-			$this->exec();
-		}
+	/**
+	 * __construct
+	 *
+	 * Constructor of the class.
+	 *
+	 * @access public
+	 */
+	public function __construct() {
+		$this->init();
 	}
+
+	/**
+	 * init
+	 *
+	 * Creates the curl handle and configures common headers for the requests.
+	 *
+	 * @access private
+	 */
+	private function init() {
+		$headers = array(
+			'Accept: application/json',
+			'Content-Type: application/json',
+		);
+		$this->handle = curl_init();
+		curl_setopt($this->handle, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($this->handle, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($this->handle, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($this->handle, CURLOPT_SSL_VERIFYPEER, false);
+	}
+
+	/**
+	 * close
+	 *
+	 * Closes the curl handle.
+	 *
+	 * @access private
+	 */
+	private function close() {
+		curl_close($this->handle);
+	}
+
+	/**
+	 * exec
+	 *
+	 * Executes the request and saves the http response, http response code and error message (if any).
+	 *
+	 * @access private
+	 */
+	private function exec() {
+		$this->response = json_decode(curl_exec($this->handle));
+		$this->http_code = curl_getinfo($this->handle, CURLINFO_HTTP_CODE);
+		$this->error = curl_error($this->handle);
+	}
+
+	/**
+	 * GET
+	 *
+	 * Implements the GET method.
+	 *
+	 * @param string $url        URL to request.
+	 * @param string $url_params URL parameters to send.
+	 *
+	 * @access public
+	 */
+	public function GET($url, $url_params='') {
+		curl_setopt($this->handle, CURLOPT_URL, $url . '?' . $url_params);
+		$this->exec();
+	}
+
+	/**
+	 * POST
+	 *
+	 * Implements the POST method.
+	 *
+	 * @param string $url        URL to request.
+	 * @param array  $data_array Array containing the data to send.
+	 *
+	 * @access public
+	 */
+	public function POST($url, $data_array) {
+		$data = json_encode($data_array);
+		curl_setopt($this->handle, CURLOPT_URL, $url);
+		curl_setopt($this->handle, CURLOPT_POST, true);
+		curl_setopt($this->handle, CURLOPT_POSTFIELDS, $data);
+		$this->exec();
+	}
+
+	/**
+	 * PUT
+	 *
+	 * Implements the PUT method.
+	 *
+	 * @param string $url        URL to request.
+	 * @param array  $data_array Array containing the data to send.
+	 *
+	 * @access public
+	 */
+	public function PUT($url, $data_array) {
+		$data = json_encode($data_array);
+		curl_setopt($this->handle, CURLOPT_URL, $url);
+		curl_setopt($this->handle, CURLOPT_CUSTOMREQUEST, 'PUT');
+		curl_setopt($this->handle, CURLOPT_POSTFIELDS, $data);
+		$this->exec();
+	}
+
+	/**
+	 * DELETE
+	 *
+	 * Implements the DELETE method.
+	 *
+	 * @param string $url URL to request.
+	 *
+	 * @access public
+	 */
+	public function DELETE($url) {
+		curl_setopt($this->handle, CURLOPT_URL, $url);
+		curl_setopt($this->handle, CURLOPT_CUSTOMREQUEST, 'DELETE');
+		$this->exec();
+	}
+}
 ?>
--- protected/utils/TMDBClient.php
+++ protected/utils/TMDBClient.php
@@ -1,37 +1,180 @@
 <?php
-	// Simple TMDB (The Movie Database) REST Client (limited to the functions needed)
-	// Author: Daniel Garcia
-	// API docs: http://docs.themoviedb.apiary.io/
+/**
+* TMDBClient
+*
+* TMDBClient: Simple TMDB (The Movie Database) REST Client.
+* Notice its methods are limited to the ones needed for this project.
+* API docs: http://docs.themoviedb.apiary.io/
+*
+* @uses      RESTClient
+*
+* @author    Daniel Garcia <daniel@danielgarcia.co>
+*/
+class TMDBClient extends RESTClient {
 
-	class TMDBClient extends RESTClient {
-		public $config;
+	/**
+	 * $config
+	 *
+	 * @var mixed
+	 *
+	 * @access public
+	 */
+	public $config;
 
-		public function __construct($api_root_url, $api_key) {
-			parent::__construct($api_root_url, $api_key);
-			// Get some config parameters
-			$this->getConfiguration();
-			$this->config = $this->response;
-		}
-		public function getConfiguration() {
-			$service = 'configuration';
-			$this->GET($service);
-		}
+	/**
+	 * $api_root_url
+	 *
+	 * @var string
+	 *
+	 * @access public
+	 */
+	public $api_root_url;
 
-		public function searchPerson($query) {
-			$service = 'search/person';
-			$query = "query=".urlencode($query);
-			$this->GET($service, $query);
-		}
+	/**
+	 * $api_key
+	 *
+	 * @var string
+	 *
+	 * @access public
+	 */
+	public $api_key;
 
-		public function getMoviesByPerson($person_id) {
-			$service = 'person/'.urlencode($person_id).'/credits';
-			$this->GET($service);
-		}
+	/**
+	 * __construct
+	 *
+	 * Constructor of the class.
+	 *
+	 * @param string $api_root_url API Root URL.
+	 * @param string $api_key      API Key.
+	 *
+	 * @access public
+	 */
+	public function __construct($api_root_url, $api_key) {
+		$this->api_root_url = $api_root_url;
+		$this->api_key = $api_key;
+
+		parent::__construct();
+
+		// Get some config parameters from TMDB
+		$this->getConfiguration();
+		$this->config = $this->response;
 	}
 
-	// Auxiliar function to sort movies by release date
-	function release_date_cmp($a, $b) {
-		return strcmp($b->release_date, $a->release_date);
+	/**
+	 * GET
+	 *
+	 * Overrides the GET method to handle the API root url and API key.
+	 *
+	 * @param string $service_name Name (path) of the service.
+	 * @param string $query_string Query parameters.
+	 *
+	 * @access public
+	 */
+	public function GET($service_name, $url_params='') {
+		$url = $this->api_root_url . $service_name;
+		$url_params = $url_params . '&api_key=' . $this->api_key;
+		parent::GET($url, $url_params);
 	}
 
+	/**
+	 * POST
+	 *
+	 * Overrides the POST method to handle the API root url and API key.
+	 *
+	 * @param string $service_name Name (path) of the service.
+	 * @param array  $data_array   Array containing the data to send.
+	 *
+	 * @access public
+	 */
+	public function POST($service_name, $data_array) {
+		$url = $this->api_root_url . $service_name;
+		$data_array['api_key']=$this->api_key;
+		parent::POST($url, $data_array);
+	}
+
+	/**
+	 * PUT
+	 *
+	 * Overrides the PUT method to handle the API root url and API key.
+	 *
+	 * @param string $service_name Name (path) of the service.
+	 * @param array  $data_array   Array containing the data to send.
+	 *
+	 * @access public
+	 */
+	public function PUT($service_name, $data_array) {
+		$url = $this->api_root_url . $service_name;
+		$data_array['api_key']=$this->api_key;
+		parent::PUT($url, $data_array);
+	}
+
+	/**
+	 * DELETE
+	 *
+	 * Overrides the DELETE method to handle the API root url and API key.
+	 *
+	 * @param string $service_name Name (path) of the service.
+	 *
+	 * @access public
+	 */
+	public function DELETE($service_name) {
+		$url = $this->api_root_url . $service_name;
+		parent::DELETE($url);
+	}
+
+	/**
+	 * getConfiguration
+	 *
+	 * Requests the configuration parameters from TMDB
+	 *
+	 * @access public
+	 */
+	public function getConfiguration() {
+		$service = 'configuration';
+		$this->GET($service);
+	}
+
+	/**
+	 * searchPerson
+	 *
+	 * @param string $query Requests the persons matching the given name.
+	 *
+	 * @access public
+	 */
+	public function searchPerson($query) {
+		$service = 'search/person';
+		$query = "query=".urlencode($query);
+		$this->GET($service, $query);
+	}
+
+	/**
+	 * getMoviesByPerson
+	 *
+	 * Requests the movies with credits matching the given person id.
+	 *
+	 * @param string $person_id Description.
+	 *
+	 * @access public
+	 */
+	public function getMoviesByPerson($person_id) {
+		$service = 'person/'.urlencode($person_id).'/credits';
+		$this->GET($service);
+	}
+}
+
+/**
+ * release_date_cmp
+ *
+ * Auxiliar function to sort movies by release date.
+ *
+ * @param object $movie_a Movie 'a'.
+ * @param object $movie_b Movie 'b'.
+ *
+ * @access public
+ *
+ * @return int Returns < 0 if $movie_b->release_date is less than $movie_a->release_date; > 0 if $movie_b->release_date is greater than $movie_a->release_date, and 0 if they are equal.
+ */
+function release_date_cmp($movie_a, $movie_b) {
+	return strcmp($movie_b->release_date, $movie_a->release_date);
+}
 ?>
